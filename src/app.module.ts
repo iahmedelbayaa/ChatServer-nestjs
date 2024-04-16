55eacf3c90d3826085f599f431b6e25babd08644
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-17 00:02:31 +0200

    Docs user story and tested it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URI,
+      synchronize: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api');
+  await app.listen(configService.get('PORT') || 3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
