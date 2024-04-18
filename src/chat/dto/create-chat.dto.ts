35b1d4c8bacc3d874fcc5704fe1585fd21e3693f
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-04-18 06:19:42 +0200

    Docs Chat resource & implement socket io

--- src/chat/dto/create-chat.dto.ts
+++ src/chat/dto/create-chat.dto.ts
@@ -1,1 +1,9 @@
-export class CreateChatDto {}
+import { UserEntity } from "src/users/entities/user.entity";
+
+export class CreateChatDto {
+    id : number;
+    message: string;
+    user: UserEntity;
+    createdAt: Date;
+    updatedAt: Date;
+}
