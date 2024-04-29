df1ce8d15a0263fa81a634cfb7fd77d24e31420c
Ahmed Elbayaa <49652597+iahmedelbayaa@users.noreply.github.com>
2024-04-29 18:41:27 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe, Logger } from '@nestjs/common';
+import * as socketio from 'socket.io';
+
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');  
+  app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(configService.get('PORT') || 3000);
   logger.log(`Application is running on: ${await app.getUrl()}`);
 }
